e6e188bd6d1a821680cdf019e17506dc71c22e90
Newton Migosi <newtonmigosi@gmail.com>
2018-04-26 14:44:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ function positionToInteger(p) {
 function start() {
   window.addEventListener('keydown', moveDodger);
 
-  START.style.display = 'none'
+  START.style.display = 'none';
 
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
