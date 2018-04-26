8ccecc9c80cc9b452fe3d9ad42c84a63924a82e5
Newton Migosi <newtonmigosi@gmail.com>
2018-04-26 15:02:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function endGame() {
 
 function moveDodger(e) {
    if (e.which == LEFT_ARROW) { e.preventDefault; e.stopPropagation(); moveDodgerLeft() }
-   if (e.which == RIGHT_ARROW) { moveDodgerRight() }
+   if (e.which == RIGHT_ARROW) {e.preventDefault; e.stopPropagation(); moveDodgerRight() }
 }
 
 function moveDodgerLeft() {
